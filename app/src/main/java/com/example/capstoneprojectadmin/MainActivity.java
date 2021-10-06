8c744c4137843f36c14d31c992b05266272f50e3
jianlin070 <jianlinlim@gmail.com>
2021-10-07 00:20:33 +0800

    Login page

---
+++ app/src/main/java/com/example/capstoneprojectadmin/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.capstoneprojectadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+}
